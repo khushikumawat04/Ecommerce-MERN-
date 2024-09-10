f9ab5d18f5cce29d7be2e93b083994789bfed22d
khushikumawat04 <kkhushikumawat04@gmail.com>
2024-09-10 21:59:49 +0530

    Added frontend React files

--- src/index.js
+++ src/index.js
@@ -2,16 +2,27 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import App from './App';
-import reportWebVitals from './reportWebVitals';
+import { Auth } from './context/Auth';
+// import { BrowserRouter } from 'react-router-dom';
+// import { createRoot } from 'react-dom/client';
+import 'antd/dist/reset.css';
+import { SearchProvider } from './context/Search';
+import { CartProvider } from './context/Cart';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
-);
+  // ReactDOM.render(
+    <Auth>
+      <SearchProvider>
+        <CartProvider>
+    <React.StrictMode>
+     <App/>
+    </React.StrictMode>
+    </CartProvider>
+    </SearchProvider>
+    </Auth>
+    // document.getElementById('root')
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+  );
+
+
